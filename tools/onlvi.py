1ef4c4168c634370bce6ecfc612bea12a143c047
nicwu-cel <nicwu@celestica.com>
2020-06-21 20:01:19 -0700

    add version and modify baud rateto be 9600 and remove hw-management

--- tools/onlvi.py
+++ tools/onlvi.py
@@ -3,7 +3,7 @@ class OnlVersionImplementation(object):
     PRODUCTS = [
         {
             "id" : "ONL",
-            "version": "2.0.0"
+            "version": "0.8.0"
             }
         ]
 
