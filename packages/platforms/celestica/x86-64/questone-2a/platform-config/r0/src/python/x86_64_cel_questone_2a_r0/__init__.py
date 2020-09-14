b098a44e3cd16de9a1353ab54644a6c27913570d
Peerapong Jaipakdee <pjaipakdee@celestica.com>
2020-09-14 17:51:04 +0700

    Add unnecessary module as blacklist file.

--- packages/platforms/celestica/x86-64/questone-2a/platform-config/r0/src/python/x86_64_cel_questone_2a_r0/__init__.py
+++ packages/platforms/celestica/x86-64/questone-2a/platform-config/r0/src/python/x86_64_cel_questone_2a_r0/__init__.py
@@ -13,6 +13,23 @@ class OnlPlatform_x86_64_cel_questone_2a_r0(OnlPlatformCelestica,
         qsfp_quantity = 8
         sfp_quantity = 48
         sfp_i2c_start_bus = 2
+
+        #Celestica Blacklist file
+        blacklist_file_path="/etc/modprobe.d/celestica-blacklist.conf"
+        #Blacklist the unuse module.
+        if os.path.exists(blacklist_file_path):
+            os.system("rm {0}".format(blacklist_file_path))
+       
+        os.system("touch {0}".format(blacklist_file_path))
+        cel_paths = "/lib/modules/{0}/onl/celestica/".format(os.uname()[2])
+        cel_dirs = os.listdir(cel_paths)
+        for dir in cel_dirs:
+            full_cel_path=cel_paths+dir
+            if os.path.isdir(full_cel_path):
+                modules=os.listdir(full_cel_path)
+                for module in modules:
+                    os.system("echo 'blacklist {0}' >> {1}".format(module[0:-3],blacklist_file_path))
+
         print("Initialize and Install the driver here")
         self.insmod("questone2a_switchboard.ko")
         self.insmod("questone2a_baseboard_cpld.ko")
